9151e32ad622757733d471f95fba6f59f885e669
Kissapod <akselkotic@gmail.com>
2024-10-14 21:47:46 +0300

    change sleep time

--- test_items.py
+++ test_items.py
@@ -1,14 +1,13 @@
 import time
 from selenium.webdriver.common.by import By
 
-
 def test_add_to_cart_button_is_displayed(browser):
     # Открываем страницу товара
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
 
     # Ожидаем несколько секунд, чтобы убедиться, что страница загружена
-    time.sleep(30)
+    time.sleep(5)
 
     # Проверяем наличие кнопки добавления в корзину
     button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
